78eba54ea4f599008e0d8e045803da5fc754586e
wildux <wildux@users.noreply.github.com>
2015-05-03 11:58:09 +0200

    ADD Selections

---
+++ src/Domain/Grafos/Selections.java
@@ -0,0 +1,26 @@
+package Domain.Grafos;
+
+import java.util.ArrayList;
+
+/**
+*
+* @author Joan
+*/
+
+
+public class Selections {
+	
+	private ArrayList<String> selection;
+
+	
+	public ArrayList<String> getSelecion() {
+		return selection;
+	}
+
+	public void setSelection(ArrayList<String> selection) {
+		this.selection = selection;
+	}
+	
+	
+
+}
